aa646f9fc3d57c5b478d96ef3833b9a77adf6dab
memuna <mmharuna16@gmail.com>
2017-02-23 18:14:02 +0100

    "changed language to location"
    Signed-off-by: memuna <mmharuna16@gmail.com>

--- Greetr.js
+++ Greetr.js
@@ -4,58 +4,90 @@
 
   // console.log(global);
   // console.log($);
-  var Greetr = function (firstName, lastName, language){
+  var Greetr = function (firstName, lastName, location){
     ///return an object of the Greetr.init function using the new keyword
-    return new Greetr.init(firstName, lastName, language);
+    return new Greetr.init(firstName, lastName, location);
   }
 
   //properties hidden within the body of the IIFE and not accessible outside this code
-  var supportedLangs = ["Hausa", "Yoruba", "Igbo"];
+  // var supportedLangs = ["Hausa", "Yoruba", "Igbo"];
+
+  // var formalGreetings = {
+  //   Hausa: "Ina Kwana",
+  //   Yoruba: "E ku ojomo o",
+  //   Abia: "Igbolachi"
+  // };
 
   var greetings = {
-    Hausa: "Ina Kwana",
-    Yoruba: "E ku ojomo o",
-    Igbo: "Igbolachi"
-  };
-
-  var formalGreeting = {
-    Hausa: "Sannu",
-    Yoruba: "E nle o",
-    Igbo: "Kachifo"
-  };
+    Abia: "Kachifo",
+    Abuja: "Sannu",
+    Adamawa: "Sannu",
+    Anambra: "Kachifo",
+    AkwaIbom: "Kachifo",
+    Bauchi: "Sannu",
+    Bayelsa: "Kachifo",
+    Benue: "Sannu",
+    Borno: "Sannu",
+    CrossRiver: "Kachifo",
+    Delta: "Kachifo",
+    Ebonyi: "Kachifo",
+    Enugu: "Kachifo",
+    Edo: "Kachifo",
+    Ekiti: "E kaa bo o",
+    Gombe: "Sannu",
+    Imo: "Kachifo",
+    Jigawa: "Sannu",
+    Kaduna: "Sannu",
+    Kano: "Sannu",
+    Katsina: "Sannu",
+    Kebbi: "Sannu",
+    Kogi: "Sannu",
+    Kwara: "E kaa bo o",
+    Lagos: "E kaa bo o",
+    Nasarawa: "Sannu",
+    Niger: "Sannu",
+    Ogun: "E kaa bo o",
+    Ondo: "E kaa bo o",
+    Osun: "E kaa bo o",
+    Oyo: "E kaa bo o",
+    Plateau: "Sannu",
+    Rivers: "Kachifo",
+    Sokoto: "Sannu",
+    Taraba: "Sannu",
+    Yobe: "Sannu",
+    Zamfara: "Sannu",
+   };
 
   Greetr.prototype = {
     //where all objects with Greetr as their prototype points to
-    //the this variable here refers to all objects pointing to this prototype
+    //the *this* variable here refers to all objects pointing to this prototype
 
     getFullName: function(){
       return this.firstName + " " + this.lastName;
     },
 
-    validate: function(){
-      if(supportedLangs.indexOf(this.language) === -1){
-        throw "Greeting is not available in this language";
-      }
-    },
+    // validate: function(){
+    //   if(supportedLangs.indexOf(this.language) === -1){
+    //     return "Welcome " + this.firstName + this.lastName;
+    //   }
+    // },
 
     greeting: function(){
-      return greetings[this.language] + " " + this.firstName + "!";
+      return greetings[this.location] + " " + this.firstName + "!";
     },
 
-    formalGreeting: function(){
-      return formalGreeting[this.language] + ", " + this.getFullName();
-    },
+    // formalGreeting: function(){
+    //   return formalGreeting[this.language] + ", " + this.getFullName();
+    // },
 
-    greet: function(formal){
+    greet: function(){
       var msg;
 
-      if(formal){
-        msg = this.formalGreeting();
-      }
+      // if(formal){
+      //   msg = this.formalGreeting();
+      // }
 
-      else{
-        msg = this.greeting();
-      }
+      msg = this.greeting();
 
       if(console){
         console.log(msg);
@@ -68,14 +100,14 @@
     },
 
     //changing your preferred language
-    setLang: function(newLang){
-      this.language = newLang;
-      this.validate();
+    setLocation: function(newLocation){
+      this.location = newLocation;
+      // this.validate();
       return this;
     },
 
     //adding JQuery Support
-    HTMLGreeting : function(selector, formal){
+    HTMLGreeting : function(selector){
       var msg;
       if(!$){
         throw "JQuery not loaded";
@@ -84,13 +116,11 @@
         throw "Invalid selector";
       }
 
-      if(formal){
-        msg = this.formalGreeting();
-      }
+      // if(formal){
+      //   msg = this.formalGreeting();
+      // }
 
-      else{
-        msg = this.greeting();
-      }
+      msg = this.greeting();
 
       $(selector).html(msg);
 
@@ -99,14 +129,14 @@
   };
 
 //the actual object is built here using the new keyword retured in the Greetr function above
-  Greetr.init = function (firstName, lastName, language){
+  Greetr.init = function (firstName, lastName, location){
     //function consctructor that builds the Greetr.init object
     var self = this;
     self.firstName =  firstName || ""; //defaults to empty string
     self.lastName = lastName || ""; //defaults to empty string
-    self.language = language || "Hausa"; //defaults to empty string if no argument is entered
+    self.location = location || ""; //defaults to empty string if no argument is entered
 
-    self.validate();
+    // self.validate();
   }
 
 //this trick is bowrrowed from JQuery so we don't have to use the new keyword when using this library
--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-//gets a new object here. (the architecture allows us to not have the 'new keyword here')
-g = G$("john", "doe");
+//gets a new object here. (the architecture allows us to not have the 'new' keyword here)
+// g = G$("john", "doe");
 
-//use our chainable methods
-g.greet(true).setLang("Igbo").greet(true);
+// //use our chainable methods
+// g.greet(true).setLang("Igbo").greet(true);
 
-console.log();
+// console.log();
 
-//let's use our object on the click o f the login button
-$('#login').click(function(){
+//let's use our object on the click of the login button
+$('#submit').click(function(){
 
-  var loginGreetr = G$("Muna", "Haruna");
+  var loginGreetr = G$($('#firstname').val(), $('#lastname').val(), $('#location').val());
 
 //hide the login on the screen
-  $('#logindiv').hide();
+  $('#submitdiv').hide();
 
 //fire off an HTML greeting, passing the 'greeting' selected from the login
 // as the selector and the chosen language, and log the welcome as well
-  loginGreetr.setLang($('#lang').val()).HTMLGreeting('#greeting', true);
+  loginGreetr.setLocation($('#location').val()).HTMLGreeting('#greeting');
 
   console.log(loginGreetr);
 });
